06568462506cd03c81040e470a7cdd2bea6cb011
nonzamo <nonzamomwende57@gmail.com>
2024-07-30 18:23:52 -0400

    index.html

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,11 +1,16 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
+function Header(props){
+  return <h1>hello from the Header!</h1>;
+}
+export default Header;
 
-import Article from "../components/Article";
 
-test("displays the text 'please pass this test'", () => {
-  render(<Article />);
+test("displays the text 'hello from the Header!'", () => {
+  render(<Header />);
+  
+  screen.debug();
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
 });
